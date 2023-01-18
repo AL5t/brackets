0bf4d78b4420f7294df14ae07b0e203773c4e752
AL5t <oalsim5@gmail.com>
2023-01-19 02:01:07 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < bracketsConfig.length; i = i + 1) {
+    for (let j = 0; j < str.length; j = j + 1) {
+      if (str[j] === bracketsConfig[i][0] && str[j + 1] === bracketsConfig[i][1]) {
+        if (str.length === 2) {
+          str = '';
+        } else {
+          str = str.slice(0, j) + str.slice(j + 2, str.length);
+          j = -1;
+          i = 0;
+        }
+      }
+    }
+  }
+  return str === '';
 }
